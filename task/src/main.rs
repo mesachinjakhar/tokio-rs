536f1db53f1e05d2561889bbbde4a5c93b89bf48
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 00:56:43 +0530

    abort on JoinHandler

--- task/src/main.rs
+++ task/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::time;
+
 use tokio::task;
 
 #[tokio::main(flavor = "current_thread")]
@@ -18,4 +20,23 @@ async fn main() {
     println!("Result: {}", result.unwrap()); // now print works
 
     assert!(join.await.is_err());
+
+    let mut handles: Vec<_> = Vec::new();
+
+    handles.push(tokio::spawn(async {
+        time::sleep(time::Duration::from_secs(10)).await;
+        true;
+    }));
+
+    handles.push(tokio::spawn(async {
+        time::sleep(time::Duration::from_secs(10)).await
+    }));
+
+    for handle in &handles {
+        handle.abort();
+    }
+
+    for handle in handles {
+        assert!(handle.await.unwrap_err().is_cancelled()) // is cancelled return true if aborting is succeed  
+    }
 }
