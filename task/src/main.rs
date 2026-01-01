352609e87399e706e6af1b3ee43785956d7bed41
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 00:41:26 +0530

    joinhandler

--- task/src/main.rs
+++ task/src/main.rs
@@ -2,10 +2,11 @@ use tokio::task;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    task::spawn(async {
-    // this wont print
-    println!("Hello from task");
-});
+    // spwan and spawn_blocking return JoinHandler
+    let handler = task::spawn(async {
+        // this wont print because our main fn returns before this spawn  async task run
+        println!("Hello from task");
+    });
 
-
+    handler.await; // now print works
 }
