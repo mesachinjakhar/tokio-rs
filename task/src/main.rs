035a65667f94cc8d06474a97c6a2c799c7487c14
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 00:59:00 +0530

    is_finished on JoinHandler

--- task/src/main.rs
+++ task/src/main.rs
@@ -8,15 +8,13 @@ async fn main() {
     let handler = task::spawn(async {
         // this wont print because our main fn returns before this spawn  async task run
         println!("Hello from task");
-        return 5
+        return 5;
     });
 
-    let join = task::spawn(async {
-        panic!("task panic here")
-    });
+    let join = task::spawn(async { panic!("task panic here") });
 
-    // handler return Result 
-    let result = handler.await; 
+    // handler return Result
+    let result = handler.await;
     println!("Result: {}", result.unwrap()); // now print works
 
     assert!(join.await.is_err());
@@ -39,4 +37,18 @@ async fn main() {
     for handle in handles {
         assert!(handle.await.unwrap_err().is_cancelled()) // is cancelled return true if aborting is succeed  
     }
+
+    let handle1 = tokio::spawn(async {
+        // do some stuff here
+    });
+    let handle2 = tokio::spawn(async {
+        // do some other stuff here
+        time::sleep(time::Duration::from_secs(10)).await;
+    });
+
+    // Wait for the task to finish
+    handle2.abort();
+    time::sleep(time::Duration::from_secs(1)).await;
+    assert!(handle1.is_finished());
+    assert!(handle2.is_finished());
 }
