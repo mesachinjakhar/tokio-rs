0264b145b259f0221bd7feeec48f986e1dc169ea
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 17:36:45 +0530

    oneshot mpsc

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -2,6 +2,11 @@ use tokio::sync::oneshot;
 use tokio::sync::mpsc;
 use tokio::net::TcpStream;
 use tokio::io::{self, AsyncWriteExt};
+use Command::Increment;
+
+enum Command {
+    Increment,
+}
 
 
 #[tokio::main]
@@ -46,31 +51,69 @@ async fn main() -> io::Result<()> {
 
 
     // message passing 
-    let mut socket = TcpStream::connect("www.example.com:1234").await?;
-    let(tx, mut rx) = mpsc::channel(100);
+    // let mut socket = TcpStream::connect("www.example.com:1234").await?;
+    // let(tx, mut rx) = mpsc::channel(100);
+
+    // for _ in 0..10 {
+    //     let tx = tx.clone();
+
+    //     tokio::spawn(async move {
+    //         tx.send(&b"data to write"[..]).await.unwrap();
+    //     });
+    // }
+
+    // drop(tx);
+
+    // while let Some(res) = rx.recv().await {
+    //     socket.write_all(res).await?;
+    // }
+
+    // Ok(())
+
+
+    let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<u64>)>(100);
+    tokio::spawn(async move {
+        let mut counter: u64 = 0;
+
+        while let Some((cmd, response)) = cmd_rx.recv().await {
+            match cmd {
+                Increment => {
+                    let prev = counter;
+                    counter += 1;
+                    response.send(prev).unwrap();
+                }
+            }
+        }
+    });
+
+    let mut join_handles = vec![];
+
 
     for _ in 0..10 {
-        let tx = tx.clone();
+        let cmd_tx = cmd_tx.clone();
 
-        tokio::spawn(async move {
-            tx.send(&b"data to write"[..]).await.unwrap();
-        });
+        join_handles.push(tokio::spawn(async move {
+            let (resp_tx, resp_rx) = oneshot::channel();
+
+            cmd_tx.send((Increment, resp_tx)).await.ok().unwrap();
+            let res = resp_rx.await.unwrap();
+
+            println!("previous value ={}", res);
+        }))
     }
 
-    drop(tx);
-
-    while let Some(res) = rx.recv().await {
-        socket.write_all(res).await?;
+    for join_handle in join_handles.drain(..) {
+        join_handle.await.unwrap();
     }
 
     Ok(())
 }
 
 
-async fn some_computation2(i: u32) -> String {
-    format!("the result of computation {}", i)
-}
+// async fn some_computation2(i: u32) -> String {
+//     format!("the result of computation {}", i)
+// }
 
-async fn some_computation() -> String {
-    "represents the result of the computation".to_string()
-}
+// async fn some_computation() -> String {
+//     "represents the result of the computation".to_string()
+// }
