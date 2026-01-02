b995c561e1abf12cdaa7c7ff60c5d2e6979b8107
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 17:41:22 +0530

    RecvError

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -71,39 +71,52 @@ async fn main() -> io::Result<()> {
     // Ok(())
 
 
-    let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<u64>)>(100);
-    tokio::spawn(async move {
-        let mut counter: u64 = 0;
+    // let (cmd_tx, mut cmd_rx) = mpsc::channel::<(Command, oneshot::Sender<u64>)>(100);
+    // tokio::spawn(async move {
+    //     let mut counter: u64 = 0;
 
-        while let Some((cmd, response)) = cmd_rx.recv().await {
-            match cmd {
-                Increment => {
-                    let prev = counter;
-                    counter += 1;
-                    response.send(prev).unwrap();
-                }
-            }
-        }
+    //     while let Some((cmd, response)) = cmd_rx.recv().await {
+    //         match cmd {
+    //             Increment => {
+    //                 let prev = counter;
+    //                 counter += 1;
+    //                 response.send(prev).unwrap();
+    //             }
+    //         }
+    //     }
+    // });
+
+    // let mut join_handles = vec![];
+
+
+    // for _ in 0..10 {
+    //     let cmd_tx = cmd_tx.clone();
+
+    //     join_handles.push(tokio::spawn(async move {
+    //         let (resp_tx, resp_rx) = oneshot::channel();
+
+    //         cmd_tx.send((Increment, resp_tx)).await.ok().unwrap();
+    //         let res = resp_rx.await.unwrap();
+
+    //         println!("previous value ={}", res);
+    //     }))
+    // }
+
+    // for join_handle in join_handles.drain(..) {
+    //     join_handle.await.unwrap();
+    // }
+
+    // Ok(())
+
+    let (tx,rx) = oneshot::channel::<u32>();
+
+    tokio::spawn(async move {
+        drop(tx)
     });
 
-    let mut join_handles = vec![];
-
-
-    for _ in 0..10 {
-        let cmd_tx = cmd_tx.clone();
-
-        join_handles.push(tokio::spawn(async move {
-            let (resp_tx, resp_rx) = oneshot::channel();
-
-            cmd_tx.send((Increment, resp_tx)).await.ok().unwrap();
-            let res = resp_rx.await.unwrap();
-
-            println!("previous value ={}", res);
-        }))
-    }
-
-    for join_handle in join_handles.drain(..) {
-        join_handle.await.unwrap();
+    match rx.await {
+        Ok(_) => panic!("this does not happen"),
+        Err(_) => println!("the sender dropped")
     }
 
     Ok(())
