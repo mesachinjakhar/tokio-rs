09fc815998cff7c2ee74f293be75a2da5bacbd64
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 13:27:43 +0530

    mspc

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -1,4 +1,5 @@
 use tokio::sync::oneshot;
+use tokio::sync::mpsc;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -20,6 +21,28 @@ async fn main() {
 
     // Wait for the computation result
     let res = join_handle.await.unwrap();
+
+    //mpsc
+    // used for multiper producer single consumer
+    // we can produce message from either 1 task or many other different tasks
+
+    let (tx, mut rx) = mpsc::channel(100);
+
+    tokio::spawn(async move {
+        for i in 0..10 {
+            let res = some_computation2(i).await;
+            tx.send(res).await.unwrap();
+        }
+    });
+
+    while let Some(res) = rx.recv().await {
+        println!("got = {}", res);
+    }
+}
+
+
+async fn some_computation2(i: u32) -> String {
+    format!("the result of computation {}", i)
 }
 
 async fn some_computation() -> String {
