6ca67c5286f3f88cb00528b7205c54b48b409553
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-02 13:18:32 +0530

    using joinhandler

--- sync/src/main.rs
+++ sync/src/main.rs
@@ -6,17 +6,22 @@ async fn main() {
 
     tokio::spawn(async move {
         let res = some_computation().await;
+        println!("computation completed");
         tx.send(res).unwrap();
-
     });
 
-
+    println!("awaiting res");
     let res = rx.await.unwrap();
     println!("result is: {}", res);
-    
+
+    // using join handler
+
+    let join_handle = tokio::spawn(async move { some_computation().await });
+
+    // Wait for the computation result
+    let res = join_handle.await.unwrap();
 }
 
 async fn some_computation() -> String {
     "represents the result of the computation".to_string()
 }
-
