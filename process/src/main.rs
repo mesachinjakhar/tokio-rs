4c3fde1de88c9094d6a33d7515e7bddbc888cfbf
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-03 14:07:49 +0530

    process

---
+++ process/src/main.rs
@@ -0,0 +1,16 @@
+use tokio::process::Command;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // The usage is similar as with the standard library's `Command` type
+    let mut child = Command::new("echo")
+        .arg("hello")
+        .arg("world")
+        .spawn()
+        .expect("failed to spawn");
+
+    // Await until the command completes
+    let status = child.wait().await?;
+    println!("the command exited with: {}", status);
+    Ok(())
+}
