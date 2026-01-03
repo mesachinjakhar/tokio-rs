81431ce2df08d536e633c33248d22352b84eb762
Sachin Jakhar <mesachinjakhar@gmail.com>
2026-01-03 16:35:39 +0530

    hup signal

--- signal/src/main.rs
+++ signal/src/main.rs
@@ -1,8 +1,17 @@
 use tokio::signal;
+use tokio::signal::unix::{signal, SignalKind};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>{
-    signal::ctrl_c().await;
-    println!("ctrl-c recieved");
-    Ok(())
+    // signal::ctrl_c().await;
+    // println!("ctrl-c recieved");
+    // Ok(())
+
+    let mut stream = signal(SignalKind::hangup())?;
+
+    loop {
+        stream.recv().await;
+        println!("got signal HUP");
+    }
 }
+
